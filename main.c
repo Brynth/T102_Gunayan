317be5d8615d70c9cdee9e5c84b66a3581efdf47
Brynth Dave Gunayan <89186006+Brynth@users.noreply.github.com>
2021-09-24 22:40:09 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+   int num1;
+   int num2;
+   int num3;
+   int result;
+
+   printf("input the fisrt number:");
+   scanf("%d",&num1);
+   printf("input the second number:");
+   scanf("%d",&num2);
+   printf("input the third number:");
+   scanf("%d",&num3);
+   //divide and times
+   result = num1 / num2 * num3;
+   printf("\n%d divide %d * %d is %d",num1,num2,num3,result);
+
+
+
+
+
+}
