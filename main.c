216650d20db447a65b15d8b3840edfcd0b24754a
Brynth Dave Gunayan <89186006+Brynth@users.noreply.github.com>
2021-11-04 14:38:33 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,55 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
-  int choices, price, payment,change;
-  printf("please pick your order");
-  printf("\n[1].NOVA (36.00)\n[2].ICE CREAM(25.00)\n[3].COKE ZERO(67.00)\nChoice:");
-  scanf("%d",&choices);
 
-  switch(choices){
+int main()
+{
+    for(int i = 1; i < 10; i++)
+    {
+        for(int b = i; b >= 1; b--)
+        {
+            printf("%d", b);
+        }
+    printf("\n");
+    }
+     return 0;
+}
 
-   case 1:
-       price =36;
-       printf("please input payment:");
-       scanf("%d", &payment);
-       if(payment >= price){
-       printf("purchase complete! heres your NOVA...THANK YOU PLEASE COME AGAIN^\n");
-       change = payment-price;
-       printf("\nChange: %d\n", change);
-}
-else{
-    printf("not enough funds! please try again");
-}
-    break;
 
-   case 2:
-       price=25;
-       printf("please input payment:");
-       scanf("%d",&payment);
-       if(payment >= price){
-       printf("purchase complete! heres your ICE CREAM...THANK YOU PLEASE COME AGAIN^\n");
-       change = payment-price;
-       printf("\nChange: %d\n", change);
-}
-else{
-    printf("not enough funds! please try again");
-}
-    break;
-
-   case 3:
-       price=67;
-       printf("please input payment:");
-       scanf("%d",&payment);
-       if(payment >= price){
-       printf("purchase complete! heres your COKE ZERO...THANK YOU PLEASE COME AGAIN^\n");
-       change = payment-price;
-       printf("\nChange: %d\n", change);
-}
-else{
-    printf("not enough funds! please try again");
-}
-   break;
-}
-    return 0;
-}
